90a8f2c05bcb623e35e67043ceb44a8c178a8d6d
Glitch (nasal-death) <none>
2020-01-01 04:24:33 +0000

    🏛💐 Checkpoint ./package.json:2958505/1159 ./server.js:2958505/560

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const userSchema = mongoose.Schema({
   userId: String
 });
 //sipher the username to get the Id
-userSchema.plugin(encipher, {
-  fields: "userId",
-  secret: process.env.MY_SECRET
-});
+
 const exerciceSchema = mongoose.Schema({
   user: { userSchema },
   description: { type: String, required: true },
@@ -37,43 +34,49 @@ app.post("/api/exercise/new-user", (req, res) => {
     if (usersfound === null) {
       const user = new userColModel({
         username: username,
-        userId: username
+        userId: cipher.encrypt(username)
       });
-      user.save();
+      user.save((err)=> {
+        if(!err){
+          res.json({username:username,userId:cipher.encrypt(username)});
+        }
+      });
     } else {
       console.log("Name exists already");
+      res.json({username:"User name exists"});
     }
   });
 });
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/views/index.html");
-// });
 
-// // Not found middleware
-// app.use((req, res, next) => {
-//   return next({ status: 404, message: "not found" });
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({ status: 404, message: "not found" });
+});
 
 // // Error Handling middleware
-// app.use((err, req, res, next) => {
-//   let errCode, errMessage;
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
 
-//   if (err.errors) {
-//     // mongoose validation error
-//     errCode = 400; // bad request
-//     const keys = Object.keys(err.errors);
-//     // report the first validation error
-//     errMessage = err.errors[keys[0]].message;
-//   } else {
-//     // generic or custom error
-//     errCode = err.status || 500;
-//     errMessage = err.message || "Internal Server Error";
-//   }
-//   res
-//     .status(errCode)
-//     .type("txt")
-//     .send(errMessage);
-// });
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || "Internal Server Error";
+  }
+  res
+    .status(errCode)
+    .type("txt")
+    .send(errMessage);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
